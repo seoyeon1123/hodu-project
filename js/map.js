b191e1fe8b72428a4b48da2205e7c44ffad187ea
seoyeon1123 <woghks072@gmail.com>
2024-01-23 00:22:46 +0900

    refactor 버그 코드 수정2

--- js/map.js
+++ js/map.js
@@ -1,27 +1,8 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const container = document.getElementById('map');
-    const options = {
-        center: new kakao.maps.LatLng(33.4423464, 126.5714548),
-        level: 3
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = {
+        center: new kakao.maps.LatLng(33.4423464, 126.5714548), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
     };
 
-    const map = new kakao.maps.Map(container, options);
-
-    // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
-    const mapTypeControl = new kakao.maps.MapTypeControl();
-
-    // 지도에 컨트롤을 추가해야 지도위에 표시됩니다
-    map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
-
-    // 지도 확대 축소를 제어할 수 있는 줌 컨트롤을 생성합니다
-    const zoomControl = new kakao.maps.ZoomControl();
-    map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
-
-    const markerPosition = new kakao.maps.LatLng(33.4423464, 126.5714548);
-    const marker = new kakao.maps.Marker({
-        position: markerPosition
-    });
-
-    marker.setMap(map);
-});
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption); 
